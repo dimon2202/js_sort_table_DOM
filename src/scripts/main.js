2dc773751b9c4337a6cb7d0940b7ff8ead37e259
Dima Sidenko <sidenkodima2202@gmail.com>
2026-01-07 18:55:19 +0100

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,38 @@
 'use strict';
 
 // write code here
+const table = document.querySelector('table');
+const tbody = table.querySelector('tbody');
+const thead = table.querySelector('thead');
+const tfoot = table.querySelector('tfoot');
+
+const parseSalary = (srt) => {
+  return +srt.split('').slice(1).join('').split(',').join('');
+};
+
+const sortColumn = (th) => {
+  const sorted = [...tbody.children].sort((tr1, tr2) => {
+    const a = tr1.cells[th.cellIndex].innerText;
+    const b = tr2.cells[th.cellIndex].innerText;
+
+    if (th.innerText === 'Salary') {
+      return parseSalary(a) - parseSalary(b);
+    }
+
+    return a.localeCompare(b);
+  });
+
+  return sorted.map((tr) => tbody.appendChild(tr));
+};
+
+thead.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  sortColumn(th);
+});
+
+tfoot.addEventListener('click', (e) => {
+  const th = e.target.closest('th');
+
+  sortColumn(th);
+});
